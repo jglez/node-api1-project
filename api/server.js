877cf677c367c95c0665346b2739d7c176dee462
jglez <jglez.cs@gmail.com>
2021-03-08 21:53:52 -0600

    Setup server and added test endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
-// BUILD YOUR SERVER HERE
+/***** BUILD YOUR SERVER HERE *****/
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// 1. Import Express from node_modules
+const express = require('express')
+
+// 2. Initialize web server
+const server = express()
+
+// 3. Teach Express how to read payload from requests
+server.use(express.json())
+
+// 4. Build Endpoints
+server.use('/api/users', (req, res) => {
+  res.status(200).json({ message: 'API Users' })
+})
+
+
+
+
+
+// EXPORT YOUR SERVER instead of {}
+module.exports = server;
