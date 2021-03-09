40f7a34093bf895cb2a5ab20d849c595f4d89944
jglez <jglez.cs@gmail.com>
2021-03-08 23:30:36 -0600

    Added findById end point

--- api/server.js
+++ api/server.js
@@ -26,6 +26,19 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+server.get('/api/users/:id', (req, res) => {
+
+  const id = req.params.id
+
+  User.findById(id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message })
+    })
+})
+
 
 // EXPORT YOUR SERVER instead of {}
 module.exports = server;
